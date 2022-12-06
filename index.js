80843947b169ae7808b898f49864c52a8aa1933f
Vedant Manjrekar <vedant.28m@gmail.com>
2022-12-06 19:06:41 +0530

    built the logic required to move all lifts properly and send alert if a lift is called when all the lifts are busy.

--- index.js
+++ index.js
@@ -9,43 +9,96 @@ const steel_wall = document.getElementById("steel_wall");
 const floor_ = document.getElementById("floor_body");
 const door = document.getElementById("door");
 
-// * Lift's state.
+// // Lift's state.
 let state = {};
+
+// // number which will forever be larger than the relative distances between lifts and floors.
+let biggest_diff;
 
 console.log(state);
 
-let new_array = [];
+// // array which contains the difference between each lifts and floors.
+let relative_diff_array = [];
+
+// // array which has the difference of lifts and floor which are vacant.
+let vacant_lift_array = [];
+
+// // function to manage lifts state (which lift to call, if there are vacant lifts available.)
+function manage_lifts(state_of_lifts, floor_to_go) {
+  biggest_diff = Math.max(...relative_diff_array) + 1000;
+
+  // * iterating the state of lifts, subtracting the input from up() function and storing it into an array.
+  Object.keys(state_of_lifts).forEach((i, index) => {
+    // * if the state of closest lift to the floor is available true (meaning lift is free)
+    if (state_of_lifts[i].available === true) {
+      // * replace the value in the "relative_diff_array" array which matches index of current lift state's index, with the difference between floor and lift.
+      // ? reason being it must be having value of '0' or "100" depending whether its called first time or already called.
+      relative_diff_array.splice(
+        index,
+        1,
+        Math.abs(state_of_lifts[i].value - floor_to_go)
+      );
+    } else {
+      // * replace the value in the "relative_diff_array" array which matches index of current lift state's index, with 100.
+      // ? reason being, we call the lift based on who is nearest (smallest number difference) and if this lift becomes nearest while its doors are still opening and closing we dont want it to be called, hence a 100 for sake of a larger number.
+      relative_diff_array.splice(index, 1, biggest_diff);
+      console.warn(biggest_diff);
+    }
+  });
+}
+
+// // function to check if there are vacant lifts available.
+function is_vacant() {
+  // * Figuring out whether all lifts are booked or not, by seeing if the "relative_diff_array" has all "100"s in it or not.
+  console.warn(biggest_diff);
+  vacant_lift_array = relative_diff_array.filter(
+    (elem) => elem != biggest_diff
+  );
+
+  // * if "vacant_lift_array" has all "100"s in it, then tell user to wait and end the function there itself.
+  if (vacant_lift_array.length === 0) {
+    alert("Please wait, currently all the lifts are busy.");
+    return 1;
+  }
+}
+
+// console.log(vacant_lift_array.length, relative_diff_array);
 
 // // UP Function.
 function up(index_val) {
+  console.log(vacant_lift_array.length);
+
   console.log("up", index_val);
 
   // * size of the div with all the floors.
   const screen_size = main.offsetHeight;
 
-  // * iterating the state of lifts, subtracting the input from up() function and storing it into an array.
-  Object.keys(state).forEach((i, index) => {
-    console.log(state[i].available);
-    if (state[i].available === true) {
-      new_array.splice(index, 1, Math.abs(state[i].value - index_val));
-    } else {
-      new_array.splice(index, 1, 100);
-    }
-  });
+  manage_lifts(state, index_val);
 
-  console.log("Values after removing input", new_array);
+  // * calls function aswell as checks for the condition.
+  if (is_vacant() === 1) {
+    return;
+  }
 
-  const smallest_difference = Math.min(...new_array);
-  console.log(smallest_difference);
+  // console.log(vacant_lift_array);
+
+  console.log("Values after removing input", relative_diff_array);
+
+  // * smallest relative distance of lifts from floor
+  const smallest_difference = Math.min(...relative_diff_array);
+
+  // console.log(smallest_difference);
 
   // * first found index value, of the smallest difference in the array
-  const closest = new_array.indexOf(smallest_difference);
+  const closest = relative_diff_array.indexOf(smallest_difference);
 
   // * how long should the moving time of lift be so that, it will take 2s to reach each floor.
   const timeout = smallest_difference * 2;
 
   // * modifying the lift's state with the lift with smallest difference btween itself and floor.
   state[`lift_${closest}`].value = index_val;
+
+  // * setting lift's state to be unavailable (temporarily).
   state[`lift_${closest}`].available = false;
 
   console.log(state);
@@ -53,6 +106,7 @@ function up(index_val) {
   // // * how much to move.
   const move = screen_size / Number(floors_inp.value);
 
+  // * how much to move according to the lift button pressed.
   const how_much_move = `${move * (index_val - 1)}`;
 
   // * function determining after how long should the lift doors open.
@@ -65,12 +119,10 @@ function up(index_val) {
   setTimeout(() => {
     document.getElementById(`lift_${closest}`).firstElementChild.style.opacity =
       "0";
-    console.log("on");
-    state[`lift_${closest}`].available = true;
-    // state[`lift_${closest}`].value = index_val;
-  }, timeout * 2.5 * 1000);
 
-  // setTimeout(() => {}, smallest_difference * 1000);
+    // * changing the state of the lift to available as the doors are succesfully closed
+    state[`lift_${closest}`].available = true;
+  }, timeout * 1000 + 2.5 * 1000);
 
   // * code responsible for moving the nearest lift.
   document.getElementById(`lift_${closest}`).style.top = `-${how_much_move}px`;
@@ -80,82 +132,76 @@ function up(index_val) {
   // * code which sets the speed of lift.
   document.getElementById(
     `lift_${closest}`
-  ).style.transition = `all ${timeout}s ease-out`;
+  ).style.transition = `all ${timeout}s linear`;
 }
 
-// ! passing number in function as args and based on the number move lift.
-
 // // DOWN Function.
-function down(index) {
-  if (index == undefined) {
-    alert("wait");
-  }
-  console.log("click");
-
-  console.log("down", index);
+function down(index_val) {
+  // console.log("down", index);
 
   // * size of the div with all the floors.
   const screen_size = main.offsetHeight;
 
-  // * iterating the state of lifts, subtracting the input from up() function and storing it into an array.
-  Object.keys(state).forEach((i, index) => {
-    if (state[i].available === true) {
-      new_array.splice(index, 1, Math.abs(state[i].value - index));
-    } else {
-      new_array.splice(index, 1, 100);
-    }
-  });
+  manage_lifts(state, index_val);
 
-  console.log("Values after removing input", new_array);
+  console.log("Values after removing input", relative_diff_array);
+
+  if (is_vacant() === 1) {
+    return;
+  }
 
   // * smallest relative distance of lifts from floor
-  const smallest_difference = Math.min(...new_array);
+  const smallest_difference = Math.min(...relative_diff_array);
 
   // * first found index value, of the smallest difference in the array
-  const closest = new_array.indexOf(smallest_difference);
+  const closest = relative_diff_array.indexOf(smallest_difference);
 
   // * modifying the lift's state with the lift with smallest difference btween itself and floor.
-  state[`lift_${closest}`].value = index;
+  state[`lift_${closest}`].value = index_val;
   console.log(state);
 
   // * how much to move.
   const move = screen_size / Number(floors_inp.value);
 
   // * how much to move according to the lift button pressed.
-  const how_much_move = `${move * (index - 1)}`;
+  const how_much_move = `${move * (index_val - 1)}`;
 
   const timeout = smallest_difference * 2;
   console.log(timeout);
 
   // * modifying the lift's state with the lift with smallest difference btween itself and floor.
-  state[`lift_${closest}`].value = index;
+  state[`lift_${closest}`].value = index_val;
   state[`lift_${closest}`].available = false;
 
+  // * function determining after how long should the lift doors open.
   setTimeout(() => {
     document.getElementById(`lift_${closest}`).firstElementChild.style.opacity =
       "1";
   }, timeout * 1000);
 
+  // * function determining after how long should the lift doors close.
   setTimeout(() => {
     document.getElementById(`lift_${closest}`).firstElementChild.style.opacity =
       "0";
-    ("0");
 
     state[`lift_${closest}`].available = true;
-  }, timeout * 2.5 * 1000);
+  }, timeout * 1000 + 2.5 * 1000);
 
+  // * code which sets the speed of lift.
   document.getElementById(
     `lift_${closest}`
-  ).style.transition = `${timeout}s ease-out`;
+  ).style.transition = `${timeout}s linear`;
 
-  // * appying css to the lift which is closest to the floor user clicked for.
+  // * code responsible for moving the nearest lift.
   document.getElementById(`lift_${closest}`).style.top = `-${how_much_move}px`;
 
   console.log(move);
 }
 
-// * Event listener for generate button
+// // Event listener for "Generate" button
 generate_btn.addEventListener("click", () => {
+  floors_inp.inputmode = "none";
+
   // * Fetching no. of floors from user.
   const floor_value = floors_inp.value;
   const lift_value = lift_inp.value;
@@ -163,7 +209,7 @@ generate_btn.addEventListener("click", () => {
   // * Initialising all the lifts in lift state as 1.
   for (let index = 0; index < lift_value; index++) {
     state[`lift_${index}`] = { value: 1, available: true };
-    new_array.push(0);
+    relative_diff_array.push(0);
   }
 
   console.log(state);
@@ -180,36 +226,38 @@ generate_btn.addEventListener("click", () => {
   let new_floors = "";
   let new_lifts = "";
 
+  // * dynamically generating floors based on user input
   for (let index = floor_value; index > 0; index--) {
     // * code for floor.
     let floor = `
-
-                <div class="floor_body" id="floor_body_${index}">
-
-                  <div class="btns">
-
-                  <button 
-                  id="floor_btn_down_${index}" 
-                  class="buttonDown"
-                  onclick="down(${index})">
-                  </button>
-                  
-                  <button 
-                  id="floor_btn_up_${index}" 
-                  class="buttonUp" 
-                  onclick="up(${index})">
-                  </button>
-
-                  </div>
-
-                  <div class="floor floor-${index}" id="floor_${index}"> <p class="floor_number">Floor ${index}</p> </div>
-                
-                </div>
-
-            `;
+    
+    <div class="floor_body" id="floor_body_${index}">
+    
+    <div class="btns">
+    
+    <button 
+    id="floor_btn_down_${index}" 
+    class="buttonDown"
+    onclick="down(${index})">
+    </button>
+    
+    <button 
+    id="floor_btn_up_${index}" 
+    class="buttonUp" 
+    onclick="up(${index})">
+    </button>
+    
+    </div>
+    
+    <div class="floor floor-${index}" id="floor_${index}"> <p class="floor_number">Floor ${index}</p> </div>
+    
+    </div>
+    
+    `;
     new_floors = new_floors + floor;
   }
 
+  // * dynamically generating lifts based on user input
   for (let index = 0; index < lift_value; index++) {
     // * code for lift.
     let lift = `<div class="lift" id="lift_${index}">
@@ -243,10 +291,13 @@ generate_btn.addEventListener("click", () => {
   console.log(lift_box);
   console.log(`${main.offsetHeight}px`);
 
+  // * setting width of div which contains all the lifts.
   lift_box.style.width = `${main.offsetWidth}px`;
 
+  // * setting height of div which contains all the lifts.
   lift_box.style.height = `${main.offsetHeight}px`;
 
+  // * targeting "main" div which rendered initially.
   const old_lift_box = document.getElementById("main").children[1];
 
   // * replaces old lift_box with the new one (lift box with updated styles), as the new one does not rendering in the DOM.
